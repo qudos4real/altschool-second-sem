1981d8cff6f71e151f533192dec9e265b335a07b
Qudos4real <qudos4real@yahoo.com>
2022-11-04 16:07:21 +0100

    Added css

--- src/App.js
+++ src/App.js
@@ -4,24 +4,39 @@ import './App.css';
 import Reducer from './Components/Reducer';
 import Home from './Components/Home';
 import NotFound from './Components/NotFound';
+import ErrorBoundary from './Components/ErrorBoundary';
+import "../CSS/Counter.css";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 
 
 function App() {
   return (
+    // <>
+    // <Home />
+    // <Counter /> 
+    // <Reducer />
+    // <NotFound />
+    // <ErrorBoundary />
+
+    // </>
+
     <Router>
+      <ErrorBoundary >
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="counter">Counter</Link>
-        <Link to="reducer">Reducer</Link>
+        <Link to="/" className='link'>Home</Link>
+        <Link to="counter" className='link'>Counter</Link>
+        <Link to="reducer" className='link'>Reducer</Link>
+        <Link to="ErrorBoundary" className='link'>ErrorBoundary</Link>
         </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/counter" element={<Counter />} />
         <Route path="/reducer" element={<Reducer />} />
+        <Route path="ErrorBoundary" element={<ErrorBoundary />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      </ErrorBoundary>
     </Router>
   )
 }
