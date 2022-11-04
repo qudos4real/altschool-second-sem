c6cead0bec7dda98106eeb12dd583293654a6791
Qudos4real <qudos4real@yahoo.com>
2022-11-04 16:12:48 +0100

    Added CSS

---
+++ .history/src/Components/Reducer_20221104161007.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useReducer } from "react";
+import { reducer } from "../hooks/reducer.1";
+import "../CSS/Counter.css"
+
+function Reducer() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  return (
+    <div className="container">
+      <h2>Created using a useReducer</h2>
+      
+      <h3 className="label "> Quantity: </h3>
+      <h3 className="value ">{state.count}</h3>
+      <br />
+      <div>
+      <button className="btn " onClick={() => dispatch({ type: "increment" })}>Increase</button>
+      <button className="btn " onClick={() => dispatch({ type: "decrement" })}>Decrease</button>
+      <button className="btn " onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      </div>
+      <br />
+      <input
+        type="number" placeholder="Enter a number" className="input "
+        onMouseLeave={(e) => dispatch({ type: "setValue", value: e.target.value })}
+      />
+    </div>
+  );
+}
+
+export default Reducer;
